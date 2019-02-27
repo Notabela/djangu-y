0cfad59d04d01647889ab534f36d075850e3f38d
daobeng <daniel.obeng@cbsinteractive.com>
2019-02-27 15:10:59 -0500

    completes countdown

--- src/Components/Sections/Sections.js
+++ src/Components/Sections/Sections.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import NavComponent from '../Navbar/Navbar'
+import {Container, Row, Col} from 'reactstrap'
+import Countdown from 'react-countdown-now'
 import './Sections.css'
 
 const Section1 = () => {
-    const image = 'http://amediaagency.com/app/uploads/2016/06/MCF_Youth_Think_Tank_Uganda_Rwanda_2016_2791-2.jpg'
+    const image = 'https://wonderfulengineering.com/wp-content/uploads/2016/01/Africa-Wallpaper-5.jpg'
     const cssStyles = {
         backgroundImage: `url(${image})`,
         backgroundRepeat: 'no-repeat',
@@ -12,7 +14,7 @@ const Section1 = () => {
     }
 
     return (
-        <div className="section" id="section_1" style={cssStyles}>
+        <div id="section_1" style={cssStyles}>
             <NavComponent />
             <p>Speak<br/>Djangu-y</p>
         </div>
@@ -21,17 +23,63 @@ const Section1 = () => {
 
 const Section2 = () => {
     return (
-        <div className="section" id="section_2">
-            <p className="animated bounceInUp">Some Cool Animating Information will go Here</p>
-        </div>
+        <Container fluid={true}>
+            <div id="section_2">
+                <p>
+                Some Cool Animating Information will go Here
+                Some Cool Animating Information will go Here
+                Some Cool Animating Information will go Here
+                </p>
+            </div>
+        </Container>
+    )
+}
+
+const Section3 = ({countDownFrom}) => {
+
+    const renderer = ({ days, hours, minutes, seconds, completed }) => {
+        return (
+            <div id="countdown">
+            <p>Jours restant avant le lancement de l’application</p>
+            <br></br>
+            <p>{days} days, {hours} hours, {minutes} minutes and {seconds} seconds</p>
+            </div>
+        )
+    }
+
+    const csStyleLeft = {
+        background: 'yellow'
+    }
+    
+    const csStyleRight = {
+        background: 'orange'
+    }
+
+    return (
+        <Container fluid={true}>
+            <div id="section_3">
+                <Row>
+                   <Col id='section-3-left' style={csStyleLeft} sm="6">
+                   <Countdown date={countDownFrom} renderer={renderer}/>
+                   </Col>
+
+                   <Col id='section-3-right' style={csStyleRight} sm="6">
+                   Some different data here
+                   </Col> 
+                </Row>
+            </div>
+        </Container>
     )
 }
 
 const Sections = () => {
+
+    const countDownDate = new Date("April 21 2019 12:30");
     return (
         <div>
             <Section1 />
             <Section2 />
+            <Section3 countDownFrom={countDownDate}/>
         </div>
     )
 }
