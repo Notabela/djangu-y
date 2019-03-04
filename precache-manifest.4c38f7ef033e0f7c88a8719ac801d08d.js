47523d211354ac4f02e7141dd65bfca9d74bb856
daobeng <daniel.obeng@cbsinteractive.com>
2019-03-03 20:03:41 -0500

    Updates

---
+++ precache-manifest.4c38f7ef033e0f7c88a8719ac801d08d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "215c1997319ba55355a3",
+    "url": "/djangu-y/static/css/main.77ca2aef.chunk.css"
+  },
+  {
+    "revision": "215c1997319ba55355a3",
+    "url": "/djangu-y/static/js/main.215c1997.chunk.js"
+  },
+  {
+    "revision": "c46c324b9c67e135a95d",
+    "url": "/djangu-y/static/js/runtime~main.c46c324b.js"
+  },
+  {
+    "revision": "dc058c504a9bebda2b98",
+    "url": "/djangu-y/static/css/2.a2f10885.chunk.css"
+  },
+  {
+    "revision": "dc058c504a9bebda2b98",
+    "url": "/djangu-y/static/js/2.dc058c50.chunk.js"
+  },
+  {
+    "revision": "89245a44e6db89d989502cfd649d09c7",
+    "url": "/djangu-y/index.html"
+  }
+];
