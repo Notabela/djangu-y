b07f44c584d1211a95884693b8e459f299c30557
daobeng <daniel.obeng@cbsinteractive.com>
2019-03-27 01:28:00 -0400

    Updates

---
+++ precache-manifest.afe375b1da0397a4ca6c373c4aae2810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a41d37d9d6440d1a621a",
+    "url": "/djangu-y/static/css/main.e7927485.chunk.css"
+  },
+  {
+    "revision": "a41d37d9d6440d1a621a",
+    "url": "/djangu-y/static/js/main.a41d37d9.chunk.js"
+  },
+  {
+    "revision": "c46c324b9c67e135a95d",
+    "url": "/djangu-y/static/js/runtime~main.c46c324b.js"
+  },
+  {
+    "revision": "ea63ead34b5435cea2fa",
+    "url": "/djangu-y/static/css/2.a2f10885.chunk.css"
+  },
+  {
+    "revision": "ea63ead34b5435cea2fa",
+    "url": "/djangu-y/static/js/2.ea63ead3.chunk.js"
+  },
+  {
+    "revision": "0073bb472c27aedab25a62facec6fcb5",
+    "url": "/djangu-y/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/djangu-y/precache-manifest.e4e319904589f60ea0f287fed1b0a48d.js"
+  "/djangu-y/precache-manifest.afe375b1da0397a4ca6c373c4aae2810.js"
 );
 
 workbox.clientsClaim();
